b763460815ddcca9843ff23b159e6e65d9faeb43
Hitesh <hiteshsharma.2314@gmail.com>
2024-01-24 22:48:27 +0530

    added put route for user to update credentials

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import 'dotenv/config';
-import User from '../models/user.model.js';
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import z from 'zod';
+import User from '../models/user.model.js';
+import authMiddlware from '../middlewares/authMiddleware.js'
 
 const app = express();
 
@@ -37,26 +38,20 @@ router.post('/signup', async (req, res) => {
   }
 
   try {
-    bcrypt.hash(password, process.env.SALT_ROUNDS, function(err, hash) {
-      if (err) {
-        return res.json({
-          message: 'error while hashing'
-        })
-      }
+    const hash = bcrypt.hashSync(password, process.env.SALT_ROUNDS)
 
-      const createUser = User.create({
-        username,
-        password: hash,
-        firstName,
-        lastName
-      });
+    const createUser = User.create({
+      username,
+      password: hash,
+      firstName,
+      lastName
+    });
 
-      const token = jwt.sign({ userId: createUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ userId: createUser._id }, process.env.JWT_SECRET);
 
-      res.status(200).json({
-        message: 'User created successfully',
-        token: token
-      })
+    res.status(200).json({
+      message: 'User created successfully',
+      token: token
     })
   } catch (error) {
     res.json({
@@ -92,28 +87,69 @@ router.post('/signin', async (req, res) => {
       })
     }
 
-    bcrypt.compare(password, user.password, function(err, result) {
-      if (err) {
-        return res.json({
-          message: 'error while comparing hash'
-        })
-      }
+    const result = bcrypt.compareSync(password, user.password)
 
-      if (!result) {
-        return res.status(411).json({
-          message: 'invalid credentials'
-        })
-      }
-
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-
-      res.status(200).json({
-        token
+    if (!result) {
+      return res.status(411).json({
+        message: 'invalid credentials'
       })
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({
+      token
     })
   } catch (error) {
     res.status(411).json({
       message: 'Error while logging in'
+    })
+  }
+})
+
+const updateBody = z.object({
+  password: z.string().email().optional(),
+  firstName: z.string().max(50).optional(),
+  lastName: z.string().max(50).optional()
+})
+
+router.put('/', authMiddlware, async (req, res) => {
+  const userId = req.userId;
+  let { password, firstName, lastName } = req.body;
+
+  const { success } = updateBody.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: 'invalid inputs'
+    })
+  }
+
+  let updateObject = {
+    ...(password !== undefined ? { password } : {}),
+    ...(firstName !== undefined ? { firstName } : {}),
+    ...(lastName !== undefined ? { lastName } : {})
+  }
+
+  if (password) {
+    const hash = bcrypt.hashSync(password, process.env.SALT_ROUNDS)
+
+    updateBody.password = hash
+  }
+
+  try {
+
+    await User.findOneAndUpdate(
+      { _id: userId },
+      updateObject
+    )
+
+    res.status(200).json({
+      message: 'Updated successfully'
+    })
+  } catch (error) {
+    res.status(411).json({
+      message: 'Error while updateing information'
     })
   }
 })
