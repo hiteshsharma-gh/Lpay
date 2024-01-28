caada9e0ccb997ed88752be9e76ec6fae92d2a25
Hitesh <hiteshsharma.2314@gmail.com>
2024-01-28 23:30:12 +0530

    boilerplate routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <div>hello world!</div>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/send' element={<Send />} />
+      </Routes>
     </BrowserRouter>
 
   )
