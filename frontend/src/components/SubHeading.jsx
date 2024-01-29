b8d6112514093b54541d40bf715c0473ffd43158
Hitesh <hiteshsharma.2314@gmail.com>
2024-01-29 22:49:13 +0530

    add subheading component

---
+++ frontend/src/components/SubHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SubHeading(props) {
+  return (
+    <h2 className='text-gray-500 text-base text-center px-5 font-medium font-sans'>{props.subheading}</h2>
+  )
+}
+
+export default SubHeading
