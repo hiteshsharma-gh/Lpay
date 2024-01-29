e31877029dd1dfdf7f2cc16b88b8cb3e93425c78
Hitesh <hiteshsharma.2314@gmail.com>
2024-01-29 22:48:43 +0530

    add heading componenet

---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Heading(props) {
+  return (
+    <h1 className='text-black text-4xl font-bold font-sans pt-5 pb-3'>{props.heading}</h1>
+  )
+}
+
+export default Heading
