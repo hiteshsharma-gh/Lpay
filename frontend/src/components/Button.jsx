4a48916f3dad318112abf2b5e274596607e1a015
Hitesh <hiteshsharma.2314@gmail.com>
2024-01-31 22:33:03 +0530

    add navigate

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { startTransition } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Button(props) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    startTransition(() => {
+      navigate('/dashboard')
+    })
+  }
+
   return (
-    <button className='bg-black h-10 w-11/12 text-white rounded-lg mt-4 mb-3'>{props.text}</button>
+    <button onClick={handleClick} className='bg-black h-10 w-11/12 text-white rounded-lg mt-4 mb-3'>{props.text}</button>
   )
 }
 
