37d0c5096d7a2d147a016c0de35ff63456e36ef5
Hitesh <hiteshsharma.2314@gmail.com>
2024-01-31 22:33:49 +0530

    add recoil state

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { useRecoilState } from 'recoil'
+import inputBoxAtomFamily from '../atoms/inputBoxAtom.atom'
 
 function InputBox(props) {
+  const [inputValue, setInputValue] = useRecoilState(inputBoxAtomFamily(props.id))
+
+  function handleChange(e) {
+    const value = e.target.value
+    setInputValue(value)
+  }
+
   return (
     <div className='flex flex-col w-full pt-4 px-4'>
       <label className='pb-2 font-semibold text-sm' htmlFor={props.id}>{props.label}</label>
-      <input className='h-10 border border-gray-200 rounded-l-md px-2 text-sm text-neutral-400' id={props.id} name={props.id} placeholder={props.placeholder}></input>
+      <input value={inputValue} onChange={handleChange} autoComplete='off' className='h-10 border border-gray-200 rounded-l-md px-2 text-sm placeholder-neutral-400' id={props.id} name={props.id} placeholder={props.placeholder} type={props.id == 'password' ? 'password' : 'text'}></input>
     </div>
   )
 }
