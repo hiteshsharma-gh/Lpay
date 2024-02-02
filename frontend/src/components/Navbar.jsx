c4ac08e40892f9843e0cbfaf4e11fc5a59be44d7
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-02 23:38:31 +0530

    ui for Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+function Navbar(props) {
+  const logo = props.firstName[0];
 
-function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='w-screen h-fit flex justify-between border-gray-100 border-b-2 px-10 py-6'>
+      <h1 className='font-bold text-4xl '>{props.heading}</h1>
+      <div className='flex gap-3 justify-center items-center'>
+        <h3 className='font-medium text-lg'>{props.greating}</h3>
+        <button className='bg-zinc-100 w-9 h-9 text-lg rounded-full flex justify-center items-center'>{logo}</button>
+      </div>
+    </div >
   )
 }
 
