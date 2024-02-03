4bf03481d8d6de1c3f24410eda18908a114d5be4
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-03 22:42:51 +0530

    ui for balance component

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,13 @@
+import Balance from "./Balance"
 import Navbar from "./Navbar"
+import SearchBar from "./SearchBar"
 
 function Dashboard() {
   return (
     <div>
       <Navbar heading='Light Pay' firstName='User' greating='Hello, User' />
+      <Balance balance='5000' />
+      <SearchBar />
     </div>
   )
 }
