58d1b26f2a0c68f7b3246a67f4a8979035b7b260
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-05 21:59:35 +0530

    ui for searchbar component

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ function SearchBar() {
   }
 
   return (
-    <div className='w-full h-fit flex justify-center items-start gap-4 flex-col px-10'>
+    <div className='w-full h-fit flex justify-center items-start gap-4 flex-col px-10 pb-5'>
       <h2 className='font-bold text-2xl'>Users</h2>
       <input className='w-full h-10 px-3 border border-zinc-400 rounded-md' name='searchUser' onChange={onChange} placeholder='Search users...' value={searchUser}></input>
     </div>
