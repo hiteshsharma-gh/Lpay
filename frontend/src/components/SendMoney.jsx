ea251a18c3f5478012e1a545dcb97e2913e5a0dc
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-16 23:30:42 +0530

    connect to backend

--- frontend/src/components/SendMoney.jsx
+++ frontend/src/components/SendMoney.jsx
@@ -1,6 +1,58 @@
+import { useRecoilState } from "recoil"
+import amountAtom from "../atoms/amount.atom.js"
+import { useNavigate, useSearchParams } from "react-router-dom";
+import axios from "axios";
+
 function Send() {
+  const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  const to = searchParams.get("to")
+  const name = searchParams.get("name")
+
+  const logo = name.split(" ")[0][0] + name.split(" ")[1][0];
+
+  const [amount, setAmount] = useRecoilState(amountAtom)
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setAmount(value)
+  }
+
+  function handleClick() {
+    axios({
+      method: "post",
+      url: "http://localhost:8080/api/v1/account/transfer",
+      data: {
+        to,
+        amount
+      },
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+      .then(response => {
+        if (response.status == 200) {
+          navigate("/dashboard")
+        }
+      })
+  }
+
   return (
-    <div>Send</div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col h-fit w-fit rounded-lg shadow-lg shadow-gray-700/50 justify-center items-center p-7">
+        <h1 className="font-bold text-4xl pb-12">Send Money</h1>
+        <div className="flex justify-start gap-4 pb-4 items-center self-start">
+          <div className="rounded-full w-11 h-11 text-3xl text-white font-medium bg-green-500 flex justify-center items-center">{logo}</div>
+          <h2 className="text-3xl font-semibold">{name}</h2>
+        </div>
+        <div className="flex flex-col justify-start items-start gap-2 p-0 mb-5">
+          <label className="font-medium" htmlFor="Amount">Amount (in Rs)</label>
+          <input className="h-9 w-96 p-3 rounded-md border-zinc-500 border" id="Amount" name="Amount" placeholder="Enter amount" value={amount} onChange={handleChange}></input>
+        </div>
+        <button className="w-96 h-9 rounded-md bg-green-500 text-white" onClick={handleClick}>Initiate Transfer</button>
+      </div>
+    </div>
   )
 }
 
