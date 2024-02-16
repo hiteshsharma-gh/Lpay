efea0b2361f01f3bbeaa06984126229457e0ad33
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-16 23:29:58 +0530

    connect to backend

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -5,13 +5,14 @@ import Button from './Button.jsx'
 import BottomWarning from './BottomWarning.jsx'
 
 function Signup() {
+
   return (
     <div className='h-screen w-full bg-zinc-500 flex justify-center items-center '>
       <div className='bg-white h-fit w-[22rem] rounded-xl flex flex-col items-center px-0 py-0'>
         <Heading heading="Sign Up" />
         <SubHeading subheading="Enter your information to create an account" />
-        <InputBox id='firstname' label='First Name' placeholder='John' />
-        <InputBox id='lastname' label='Last Name' placeholder='Doe' />
+        <InputBox id='firstName' label='First Name' placeholder='John' />
+        <InputBox id='lastName' label='Last Name' placeholder='Doe' />
         <InputBox id='email' label='Email' placeholder='johndoe@example.com' />
         <InputBox id='password' label='Password' placeholder='' />
         <Button text='Sign Up' />
