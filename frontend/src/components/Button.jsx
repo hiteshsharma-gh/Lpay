451419ee32380aadbfdaf47da599c9b9f209038c
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-16 23:30:59 +0530

    handle all requests

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,13 +1,54 @@
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+import inputBoxAtomFamily from '../atoms/inputBoxAtom.atom.js'
 
 function Button(props) {
+  const firstName = useRecoilValue(inputBoxAtomFamily("firstName"))
+  const lastName = useRecoilValue(inputBoxAtomFamily("lastName"))
+  const email = useRecoilValue(inputBoxAtomFamily("email"))
+  const password = useRecoilValue(inputBoxAtomFamily("password"))
+
   const navigate = useNavigate();
 
-  function handleClick() {
+  async function handleClick() {
     if (props.text === "Send Money") {
-      navigate("/sendmoney")
-    } else {
-      navigate("/dashboard")
+      navigate(`/send?to=${props.id}&name=${props.firstName + " " + props.lastName}`)
+    } else if (props.text === "Sign Up") {
+      console.log(firstName, lastName, email, password)
+      axios({
+        method: "post",
+        url: "http://localhost:8080/api/v1/user/signup",
+        data: {
+          username: email,
+          password,
+          firstName,
+          lastName
+        }
+      })
+        .then((response) => {
+          if (response.status == 200) {
+            console.log(response.data)
+            localStorage.setItem("token", "Bearer " + response.data.token)
+            navigate("/dashboard")
+          }
+        })
+    } else if (props.text === "Sign In") {
+      axios({
+        method: "post",
+        url: "http://localhost:8080/api/v1/user/signin",
+        data: {
+          username: email,
+          password
+        }
+      })
+        .then((response) => {
+          if (response.status == 200) {
+            console.log(response.data)
+            localStorage.setItem("token", "Bearer " + response.data.token)
+            navigate("/dashboard")
+          }
+        })
     }
   }
 
