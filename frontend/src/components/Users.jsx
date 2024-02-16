7e34350c62d25f664cb04e5fe8048d69c3d13fa9
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-16 23:30:33 +0530

    connect to backen

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,16 +1,16 @@
 import Button from './Button'
 
 function Users(props) {
-  const logo = (props.lastName ? props.firstName[0] + props.lastName[0] : props.firstName[0]);
+  const logo = (props.lastname ? props.firstname[0] + props.lastname[0] : props.firstname[0]);
 
   return (
-    <div className='w-full h-14 px-10 flex justify-between items-center'>
+    <div className='w-full h-14 flex justify-between items-center'>
       <div className='flex items-center gap-4'>
         <div className='w-9 h-9 bg-zinc-100 font-medium rounded-full flex justify-center items-center'>{logo}</div>
-        <p className='font-semibold text-lg'>{`${props.firstName} ${props.lastName}`}</p>
+        <p className='font-semibold text-lg'>{`${props.firstname} ${props.lastname}`}</p>
       </div>
       <div className='w-32 h-fit flex justify-end'>
-        <Button text='Send Money' lastName={props.lastName} firstName={props.firstName} />
+        <Button text='Send Money' lastName={props.lastname} firstName={props.firstname} id={props.id} />
       </div>
     </div>
   )
