0e9b277fe728a0181acd12c740f3d75977af0803
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-16 23:30:25 +0530

    connect to backend

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,22 +1,19 @@
 import { useRecoilState } from "recoil"
 import Users from "./Users"
 import searchUserAtom from "../atoms/searchUser.atom"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function SearchBar() {
   const [searchUser, setSearchUser] = useRecoilState(searchUserAtom)
+  const [users, setUsers] = useState([])
 
-  let users = [];
   useEffect(() => {
-    const count = setTimeout(() => {
-      axios.get(process.env.SERVER_API_URI + `/bulk?filter=${searchUser}`)
-        .then((response) => {
-          users = response.data
-        })
-    }, 200)
-
-    return clearTimeout(count)
+    axios.get(`http://localhost:8080/api/v1/user/bulk?filter=${searchUser}`)
+      .then((response) => {
+        if (response.status == 200)
+          setUsers(response.data.users)
+      })
   }, [searchUser])
 
   function onChange(e) {
@@ -26,11 +23,11 @@ function SearchBar() {
 
   return (
     <div className='w-full h-fit flex justify-center items-start gap-2 flex-col px-10 '>
-      <h2 className='font-bold text-2xl pb-5'>Users</h2>
+      <h2 className='font-bold text-2xl pb-3'>Users</h2>
       <input className='w-full h-10 px-3 border border-zinc-400 rounded-md' name='searchUser' onChange={onChange} placeholder='Search users...' value={searchUser}></input>
-      {users.map((user) => {
-        <Users firstName={user.firstName} lastName={user.lastName} />
-      })}
+      {users.map(user => (
+        <Users firstname={user.firstName} lastname={user.lastName} key={user._id} id={user._id} />
+      ))}
     </div>
   )
 }
