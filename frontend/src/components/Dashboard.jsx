93df00df4bf87973f0f4fb6e187f96d09e2e023f
Hitesh <hiteshsharma.2314@gmail.com>
2024-02-16 23:31:09 +0530

    connect to backend

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,12 +1,31 @@
+import axios from "axios"
 import Balance from "./Balance"
 import Navbar from "./Navbar"
 import SearchBar from "./SearchBar"
+import { useEffect, useState } from "react"
 
 function Dashboard() {
+  const [balance, setBalance] = useState(0)
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/api/v1/account/balance",
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+      .then(response => {
+        if (response.status == 200) {
+          setBalance(response.data.balance)
+        }
+      })
+  }, [])
+
   return (
     <div>
-      <Navbar heading='Light Pay' firstName='User' lastName='User' greating='Hello, User' />
-      <Balance balance='5000' />
+      <Navbar heading='Light Pay' firstName='User' lastName='' greating='Hello' />
+      <Balance balance={balance} />
       <SearchBar />
     </div>
   )
